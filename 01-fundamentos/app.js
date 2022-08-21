4cd4da7594437da644d0a906a24081fb47d8f0b5
jmueseslab <JMUESES@bancodebogota.com.co>
2022-08-20 22:37:38 -0500

    Introduccion a la consola

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,6 +1,26 @@
-console.log(console.log('Hola mundo'));
+// console.log(console.log('Hola mundo'));
 
-let a = 10, b = 10, c = 10, d = 10, x = a+b;
+let a = 10,
+    b = 20,
+    c = 'hola ',
+    d = 'spyderman',
+    x = a+b;
+
+const saludo = c + d;
+console.log('%c Mis Variables', 'color:blue; font-weight:bold');
+
+console.log('a', a);
+console.log('b', b);
+console.log('c', c);
+
+console.log({a});
+console.log({b});
+console.log({c});
+
 console.log(x);
+console.warn(x);
+console.error(x);
 
-// c = 8; no se puede cambiar
+// const c = 8; no se puede cambiar
+
+console.table({a, b, c, d, x, saludo});
