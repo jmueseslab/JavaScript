b638294d049cb649c9dc91fda3d0ddc90d979a7e
jmueseslab <JMUESES@bancodebogota.com.co>
2022-09-10 18:04:45 -0500

    Depuracion y breakpoints

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,4 +1,4 @@
-// console.log(console.log('Hola mundo'));
+console.log(console.log('Hola mundo'));
 
 let a = 10,
     b = 20,
@@ -7,6 +7,7 @@ let a = 10,
     x = a+b;
 
 const saludo = c + d;
+
 console.log('%c Mis Variables', 'color:blue; font-weight:bold');
 
 console.log('a', a);
@@ -21,6 +22,7 @@ console.log(x);
 console.warn(x);
 console.error(x);
 
-// const c = 8; no se puede cambiar
+// const c = 8; no se puede cambiar*/
 
+c = 'Hola de nuevo';
 console.table({a, b, c, d, x, saludo});
