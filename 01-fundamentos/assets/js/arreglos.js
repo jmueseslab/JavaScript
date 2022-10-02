550d7445ed4677ee7642d40aa637e4a3532b75d9
jmueseslab <JMUESES@bancodebogota.com.co>
2022-10-02 16:48:27 -0500

    Arreglos

---
+++ 01-fundamentos/assets/js/arreglos.js
@@ -0,0 +1,26 @@
+// const arr = new Array(10);
+// const arr = [];
+// console.log(arr);
+
+let videoJuegos = ['Mario 3', 'Megaman', 'Chrono Trigger'];
+console.log({videoJuegos});
+console.log(videoJuegos[1]);
+
+let arregloCosas = [
+    true,
+    123,
+    'Jhon Jairo',
+    7+9,
+    function(){},
+    ()=>{},
+    {a:1},
+    ['X', 'Megaman', 'Zero', 'Dr. Light', [
+        'Dr. Willy',
+        'Woodman'
+    ]]
+];
+
+console.log({arregloCosas});
+console.log(arregloCosas[2]);
+console.log(arregloCosas[7][3]);
+console.log(arregloCosas[7][4][1]);
