7e674755848af3d411fef519fb72c5118c7eee19
jmueseslab <JMUESES@bancodebogota.com.co>
2022-10-02 16:08:48 -0500

    Palabras reservadas y nombre de variables

---
+++ 01-fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,22 @@
+console.log('Hola Mundo');
+let constante = 123;
+
+let await = 123;
+var π = Math.PI;
+console.log(π);
+
+//Underscode
+let _jj = 123;
+let objeto$ = 1234;
+let objeto123 = 456;
+let precio_99_99 = 99.9;
+//Lower camel case
+let nombrePersona = 'Jhon';
+//oper camel case
+class JuegoAnio {
+    //nombres clase
+}
+
+//LINKS
+//https://mothereff.in/js-variables#%E0%B2%A0_%E0%B2%A0
+//https://mathiasbynens.be/notes/javascript-identifiers
