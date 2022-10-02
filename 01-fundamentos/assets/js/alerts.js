306bf9ff52d9b17686a36922d118a0c474f74159
jmueseslab <JMUESES@bancodebogota.com.co>
2022-10-02 14:50:06 -0500

    modulo 2

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,22 @@
+//FORMAS DE INGRESO DE INFORMACION USUARIO
+//====================================
+//MOSTRAR MENSAJE
+//alert('Hola mundo');
+
+
+//====================================
+//PEDIR INFORMACION AL USUARIO
+
+//let nombre = prompt('¿Cuál es su nombre?', 'Sin nombre');
+// let nombre = prompt('¿Cuál es su nombre?');
+// console.log(nombre);
+// console.log('****'+nombre+'****'); // '' string vacio
+
+// null es diferente de undefined
+
+//====================================
+//CONFIRMAR AL USUARIO
+const seleccion = confirm('¿Esta seguro de borrar esto?');
+console.log(seleccion);
+
+// console.log(global);
