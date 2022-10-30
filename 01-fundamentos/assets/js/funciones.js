356c67abcf6444d8a4df1a4da0146d95827f3a0e
jmueseslab <JMUESES@bancodebogota.com.co>
2022-10-29 23:38:48 -0500

    Retorno de las funciones

--- 01-fundamentos/assets/js/funciones.js
+++ 01-fundamentos/assets/js/funciones.js
@@ -4,9 +4,14 @@ function saludar() {
     // metodo es una funcion que
     // se encuentra dentro de un objeto
     console.log('Hola Mundo');
+    return [10, 20];
+
+    // Esto nunca se ejecuta
+    console.log('código después del return');
 }
 
-saludar();
+const retornoSaludar = saludar();
+console.log(retornoSaludar[0], retornoSaludar[1]);
 
 //==================================
 //Funcion anonima
@@ -40,3 +45,30 @@ const saludarFlecha2 = (nombre) => {
 }
 
 saludarFlecha2('Nohelys');
+
+//==================================
+function sumar(a, b){
+    return a + b;
+}
+
+const sumar2 = (a, b) => {
+    return a + b;
+}
+
+// clean code - cuando se retorna en una sola linea
+// solo devolver el return
+const sumar3 = (a, b) => a + b;
+
+console.log(sumar(1,2));
+console.log(sumar2(1,2));
+console.log(sumar3(5,2));
+
+//==================================
+function getAleatorio(){
+    return Math.random();
+}
+
+const getAleatorio2 = () => Math.random();
+
+console.log(getAleatorio());
+console.log(getAleatorio2());
