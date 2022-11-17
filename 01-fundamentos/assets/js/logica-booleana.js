6d44312ca3cf11b3234f0a2ffdc209fe4f75f887
jmueseslab <JMUESES@bancodebogota.com.co>
2022-11-17 15:01:11 -0500

    Pro tip: Asignaciones con operadores

--- 01-fundamentos/assets/js/logica-booleana.js
+++ 01-fundamentos/assets/js/logica-booleana.js
@@ -38,4 +38,21 @@ console.log(regresaTrue() || regresaFalse()); // true
 console.log('4 condiciones', true || true || true || false); //false
 
 //============================================================
-console.warn(Asignaciones);
+console.warn('Asignaciones');
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = false && 'hola Mundo' && 150; // false
+const a2 = 'hola' && 'Mundo' && soyFalso && true; // false
+const a3 = soyFalso || 'ya no soy falso';
+const a4 = soyFalso || soyUndefined || soyNull || 'ya no soy falso de nuevo' || true;
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'ya no soy falso de nuevo' || true;
+console.log({a1, a2, a3, a4, a5});
+
+if(regresaFalse() && regresaTrue() && (true||false||true)){
+    console.log('Hacer algo');
+}else{
+    console.log('Hacer otra cosa');
+}
