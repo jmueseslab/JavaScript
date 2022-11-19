ca0b934a9e997c188edda4fa22297eacc0d285c9
jmueseslab <JMUESES@bancodebogota.com.co>
2022-11-19 11:14:32 -0500

    Switch

---
+++ 01-fundamentos/assets/js/switch.js
@@ -0,0 +1,14 @@
+const dia = 8;  // 0: domingo
+switch(dia){ // evalua ===
+    case 0: 
+        console.log('Domingo');
+        break;
+    case 1:
+        console.log('Lunes');
+        break;
+    case 2:
+        console.log('Martes');
+        break;
+    default:
+        console.log('No es lunes, martes o Domingo');
+}
