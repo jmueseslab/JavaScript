fa5d0b4a82b5fe1c207aa36b6de84c8023b8e7cd
jmueseslab <JMUESES@bancodebogota.com.co>
2023-01-15 20:22:58 -0500

    Pedir carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -12,6 +12,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+// Esta función crea un nurvo deck
 const createDeck = () => {
     for(let i = 2; i<=10; i++){
         for(let tipo of tipos){
@@ -24,10 +25,26 @@ const createDeck = () => {
             deck.push(esp+tipo);
         }
     }
-    console.log(deck);
+    // console.log(deck);
     deck = _.shuffle(deck);
     console.log(deck);
     return deck;
 }
 
 createDeck();
+
+// Esta funcion me permite tomar una carta
+
+const pedirCarta = () => {
+
+    if(deck.length === 0){
+        throw 'No hay cartas en el Deck';
+    }
+
+    const carta = deck.pop();
+    console.log(deck);
+    console.log(carta);//la carta debe ser de la baraja
+    return carta;
+}
+
+pedirCarta();
